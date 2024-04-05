7f9b78ac4e78a559483ca73e19745ed2828fa07d
Mateuzo <mateushcandido5@gmail.com>
2024-04-05 19:06:39 -0300

    Extraindo camada do produtor

--- src/main/java/com/mtzz/NewOrderMain.java
+++ src/main/java/com/mtzz/NewOrderMain.java
@@ -1,6 +1,7 @@
 package com.mtzz;
 
 
+import com.mtzz.service.KafkaDispatcher;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -14,45 +15,15 @@ import java.util.concurrent.ExecutionException;
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var producer = new KafkaProducer<String, String>( properties() );
-
-        for (int i = 0; i < 10; i++) {
-            var key = UUID.randomUUID().toString();
-            var value = key + "123, 01426871, 2100";
-            var email = "Welcome! We are proccessing your order!";
-
-            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
-            var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
-
-
-            producer.send(record, getCallback()).get();
-            producer.send(emailRecord, getCallback()).get();
+        try ( var kafkaDispatcher = new KafkaDispatcher() ){
+            for (int i = 0; i < 10; i++) {
+                var key = UUID.randomUUID().toString();
+                var value = key + "123, 01426871, 2100";
+                var email = "Welcome! We are proccessing your order!";
+                kafkaDispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                kafkaDispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+            }
         }
     }
 
-    protected static Callback getCallback() {
-        return (data, exception) -> {
-            if (exception != null) {
-                exception.printStackTrace();
-            }
-
-            System.out.println("SENT SUCCESS"
-                    + data.topic()
-                    + " | partition: "
-                    + data.partition()
-                    + " |offset: "
-                    + data.offset()
-                    + " |timestamp: " +
-                    data.timestamp());
-        };
-    }
-
-    private static Properties properties(){
-        var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
-        return properties;
-    }
 }
