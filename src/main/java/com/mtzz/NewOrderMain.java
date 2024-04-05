f1a00dee6a7679003bce5066af6436efec659a4b
Mateuzo <mateushcandido5@gmail.com>
2024-04-05 12:17:49 -0300

    implementando o processamento de 100 mensagens e implementando chaves para alocar mensagens em partições distintas

--- src/main/java/com/mtzz/NewOrderMain.java
+++ src/main/java/com/mtzz/NewOrderMain.java
@@ -8,6 +8,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
@@ -15,15 +16,18 @@ public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>( properties() );
 
-        var value = "123, 01426871, 2500";
-        var email = "Welcome! We are proccessing your order!";
+        for (int i = 0; i < 100; i++) {
+            var key = UUID.randomUUID().toString();
+            var value = key + "123, 01426871, 2100";
+            var email = "Welcome! We are proccessing your order!";
 
-        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
+            var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
 
 
-        producer.send(record, getCallback()).get();
-        producer.send(emailRecord, getCallback()).get();
+            producer.send(record, getCallback()).get();
+            producer.send(emailRecord, getCallback()).get();
+        }
     }
 
     protected static Callback getCallback() {
